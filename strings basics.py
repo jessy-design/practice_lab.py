447f16c57cb7d59a5436b86ab0fac6d172c4d6ac
jessy-design <jessysowntharya@gmail.com>
2026-04-22 07:38:48 +0530

    python basic

--- strings basics.py
+++ strings basics.py
@@ -1,5 +1,7 @@
-txt="Hello, World!"
-print(txt[2:5])
-print(txt.upper())
-name = "Python"
-print(f"I love {name}")
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
